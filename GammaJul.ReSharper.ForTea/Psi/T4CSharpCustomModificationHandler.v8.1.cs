0ed758859f64520b1c9499f30645304e62d5ad2f
Matt Ellis <m.t.ellis@gmail.com>
2013-11-19 16:33:41 +0000

    Support for ReSharper 8.1

---
+++ GammaJul.ReSharper.ForTea/Psi/T4CSharpCustomModificationHandler.v8.1.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Application.Settings;
+using JetBrains.ReSharper.Psi.CSharp.CodeStyle.FormatSettings;
+using JetBrains.ReSharper.Psi.CSharp.Impl.CustomHandlers;
+using JetBrains.ReSharper.Psi.Tree;
+
+namespace GammaJul.ReSharper.ForTea.Psi {
+
+	public partial class T4CSharpCustomModificationHandler {
+	    public ThisQualifierStyle GetThisQualifierStyle(ITreeNode context)
+	    {
+	        return context.GetSettingsStore().GetValue(CSharpCustomModificationHandlerDummy.ThisQualifierStyleAccessor);
+	    }
+	}
+}
