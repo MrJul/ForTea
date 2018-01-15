7a89ee796297dbe0ba1081f0ad6fe0baa33a1fbe
Matthias Koch <ithrowexceptions@gmail.com>
2018-01-15 12:01:54 +0100

    Add nuke build.

--- Build.cmd
+++
@@ -1,6 +0,0 @@
-set DeployDir=2017.2
-rmdir /s /q Deploy
-rmdir /s /q bin
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Professional\MSBuild\15.0\Bin\MSBuild.exe" Build.targets
-mkdir Deploy\%DeployDir%
-"%USERPROFILE%\.nuget\packages\NuGet.CommandLine\4.3.0\tools\nuget.exe" pack GammaJul.ReSharper.ForTea\GammaJul.ReSharper.ForTea.nuspec -NoPackageAnalysis -OutputDirectory Deploy\%DeployDir%
