459b3e3cb0d1be1971b07908875c3d78f9400ec7
Kirill <kirillgla@gmail.com>
2019-06-09 13:23:43 +0300

    Update plugin version

--- source/GammaJul.ReSharper.ForTea/Properties/AssemblyInfo.cs
+++ source/GammaJul.ReSharper.ForTea/Properties/AssemblyInfo.cs
@@ -23,4 +23,4 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright © Julien Lebosquain, 2012-2019")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.14.0.0")]
+[assembly: AssemblyVersion("1.14.0.2")]
