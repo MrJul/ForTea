d56683b9a5cdbb06906951c354c51fc36fa2682f
Kirill <kirillgla@gmail.com>
2019-06-13 12:24:37 +0300

    Add plugin icons

--- Rider.ForTea/src/main/kotlin/com/jetbrains/fortea/language/T4FileType.kt
+++ Rider.ForTea/src/main/kotlin/com/jetbrains/fortea/language/T4FileType.kt
@@ -1,10 +1,12 @@
 package com.jetbrains.fortea.language
 
+import com.intellij.icons.AllIcons
 import com.jetbrains.rider.ideaInterop.fileTypes.RiderLanguageFileTypeBase
+import javax.swing.Icon
 
 object T4FileType : RiderLanguageFileTypeBase(T4Language) {
   override fun getDefaultExtension() = "T4"
   override fun getDescription() = "T4 template file"
-  override fun getIcon() = TODO()
+  override fun getIcon(): Icon = AllIcons.FileTypes.Text
   override fun getName() = "T4"
 }
