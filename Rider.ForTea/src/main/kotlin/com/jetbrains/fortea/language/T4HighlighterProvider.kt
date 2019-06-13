3875acba79d98406f60230bd8c1f8a22286b59c7
Kirill <kirillgla@gmail.com>
2019-06-13 12:17:37 +0300

    Fix indents in kotlin files

--- Rider.ForTea/src/main/kotlin/com/jetbrains/fortea/language/T4HighlighterProvider.kt
+++ Rider.ForTea/src/main/kotlin/com/jetbrains/fortea/language/T4HighlighterProvider.kt
@@ -8,10 +8,10 @@ import com.intellij.openapi.project.Project
 import com.intellij.openapi.vfs.VirtualFile
 
 class T4HighlighterProvider : SyntaxHighlighterProvider, SyntaxHighlighterFactory() {
-    override fun getSyntaxHighlighter(project: Project?, virtualFile: VirtualFile?) = T4SyntaxHighlighter()
+  override fun getSyntaxHighlighter(project: Project?, virtualFile: VirtualFile?) = T4SyntaxHighlighter()
 
-    override fun create(fileType: FileType, project: Project?, file: VirtualFile?): SyntaxHighlighter? {
-        if (fileType !is T4FileType) return null
-        return T4SyntaxHighlighter()
-    }
+  override fun create(fileType: FileType, project: Project?, file: VirtualFile?): SyntaxHighlighter? {
+    if (fileType !is T4FileType) return null
+    return T4SyntaxHighlighter()
+  }
 }
--- Rider.ForTea/src/main/kotlin/com/jetbrains/fortea/language/T4ParserDefinition.kt
+++ Rider.ForTea/src/main/kotlin/com/jetbrains/fortea/language/T4ParserDefinition.kt
@@ -7,12 +7,12 @@ import com.jetbrains.rider.ideaInterop.fileTypes.RiderFileElementType
 import com.jetbrains.rider.ideaInterop.fileTypes.RiderParserDefinitionBase
 
 class T4ParserDefinition : RiderParserDefinitionBase(t4FileElementType, T4FileType) {
-    companion object {
-        private val t4ElementType = IElementType("T4", T4Language)
-        val t4FileElementType = RiderFileElementType(T4Language, t4ElementType)
-    }
+  companion object {
+    private val t4ElementType = IElementType("T4", T4Language)
+    val t4FileElementType = RiderFileElementType(T4Language, t4ElementType)
+  }
 
-    private val lexer = T4SyntaxHighlighter().highlightingLexer
+  private val lexer = T4SyntaxHighlighter().highlightingLexer
 
-    override fun createLexer(project: Project): Lexer = lexer
+  override fun createLexer(project: Project): Lexer = lexer
 }
