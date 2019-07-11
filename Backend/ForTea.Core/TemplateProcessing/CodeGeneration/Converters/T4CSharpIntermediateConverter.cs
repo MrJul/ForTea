14d63290693aa5a2ef7b9ba11e25572321276f8e
Kirill <kirillgla@gmail.com>
2019-07-11 14:11:50 +0300

    Refactor code converting

--- Backend/ForTea.Core/TemplateProcessing/CodeGeneration/Converters/T4CSharpIntermediateConverter.cs
+++ Backend/ForTea.Core/TemplateProcessing/CodeGeneration/Converters/T4CSharpIntermediateConverter.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using GammaJul.ForTea.Core.Psi;
 using GammaJul.ForTea.Core.TemplateProcessing.CodeCollecting;
 using GammaJul.ForTea.Core.Tree;
 using JetBrains.Annotations;
@@ -12,9 +11,9 @@ namespace GammaJul.ForTea.Core.TemplateProcessing.CodeGeneration.Converters
 		protected const string DefaultGeneratedClassName = "GeneratedTransformation";
 
 		public T4CSharpIntermediateConverter(
-			[NotNull] T4CSharpCodeGenerationIntermediateResult result,
+			[NotNull] T4CSharpCodeGenerationIntermediateResult intermediateResult,
 			[NotNull] IT4File file
-		) : base(result, file)
+		) : base(intermediateResult, file)
 		{
 		}
 
@@ -25,7 +24,7 @@ namespace GammaJul.ForTea.Core.TemplateProcessing.CodeGeneration.Converters
 
 		protected sealed override string GeneratedBaseClassName => GeneratedClassName + "Base";
 
-		protected sealed override void AppendSyntheticAttribute(T4CSharpCodeGenerationResult result)
+		protected sealed override void AppendSyntheticAttribute()
 		{
 			// Synthetic attribute is only used for avoiding completion.
 			// It is not valid during compilation,
@@ -33,58 +32,82 @@ namespace GammaJul.ForTea.Core.TemplateProcessing.CodeGeneration.Converters
 		}
 
 		protected sealed override void AppendParameterInitialization(
-			IReadOnlyCollection<T4ParameterDescription> descriptions,
-			T4CSharpCodeGenerationResult result)
+			IReadOnlyCollection<T4ParameterDescription> descriptions
+		)
 		{
-			result.AppendLine("            if (Errors.HasErrors) return;");
+			AppendIndent();
+			Result.AppendLine("if (Errors.HasErrors) return;");
 			foreach (var description in descriptions)
 			{
-				result.Append("            if (Session.ContainsKey(nameof(");
-				result.Append(description.FieldNameString);
-				result.AppendLine(")))");
-				result.AppendLine("            {");
-				result.Append("                ");
-				result.Append(description.FieldNameString);
-				result.Append(" = (");
-				result.Append(description.TypeString);
-				result.Append(") Session[nameof(");
-				result.Append(description.FieldNameString);
-				result.AppendLine(")];");
-				result.AppendLine("            }");
-				result.AppendLine("            else");
-				result.AppendLine("            {");
-				result.Append(
-					"                object data = global::System.Runtime.Remoting.Messaging.CallContext.LogicalGetData(nameof(");
-				result.Append(description.FieldNameString);
-				result.AppendLine("));");
-				result.AppendLine("                if (data != null)");
-				result.AppendLine("                {");
-				result.Append("                    ");
-				result.Append(description.FieldNameString);
-				result.Append(" = (");
-				result.Append(description.TypeString);
-				result.AppendLine(") data;");
-				result.AppendLine("                }");
-				result.AppendLine("            }");
+				AppendIndent();
+				Result.Append("if (Session.ContainsKey(nameof(");
+				Result.Append(description.FieldNameString);
+				Result.AppendLine(")))");
+				AppendIndent();
+				Result.AppendLine("{");
+				PushIndent();
+				AppendIndent();
+				Result.Append(description.FieldNameString);
+				Result.Append(" = (");
+				Result.Append(description.TypeString);
+				Result.Append(") Session[nameof(");
+				Result.Append(description.FieldNameString);
+				Result.AppendLine(")];");
+				PopIndent();
+				AppendIndent();
+				Result.AppendLine("}");
+				AppendIndent();
+				Result.AppendLine("else");
+				AppendIndent();
+				Result.AppendLine("{");
+				PushIndent();
+				AppendIndent();
+				Result.Append(
+					"object data = global::System.Runtime.Remoting.Messaging.CallContext.LogicalGetData(nameof(");
+				Result.Append(description.FieldNameString);
+				Result.AppendLine("));");
+				AppendIndent();
+				Result.AppendLine("if (data != null)");
+				AppendIndent();
+				Result.AppendLine("{");
+				PushIndent();
+				AppendIndent();
+				Result.Append(description.FieldNameString);
+				Result.Append(" = (");
+				Result.Append(description.TypeString);
+				Result.AppendLine(") data;");
+				PopIndent();
+				AppendIndent();
+				Result.AppendLine("}");
+				PopIndent();
+				AppendIndent();
+				Result.AppendLine("}");
 			}
 		}
 
-		protected override void AppendParameterDeclaration(
-			T4CSharpCodeGenerationResult result,
-			T4ParameterDescription description
-		)
+		protected override void AppendParameterDeclaration(T4ParameterDescription description)
 		{
-			result.Append("        private ");
+			AppendIndent();
+			Result.Append("private ");
 			var type = description.TypeToken;
-			if (CSharpLexer.IsKeyword(type.GetText())) result.Append("@");
-			result.AppendMapped(type);
-			result.Append(" ");
+			if (CSharpLexer.IsKeyword(type.GetText())) Result.Append("@");
+			Result.AppendMapped(type);
+			Result.Append(" ");
 			var name = description.NameToken;
-			if (CSharpLexer.IsKeyword(name.GetText())) result.Append("@");
-			result.AppendMapped(name);
-			result.Append(" => ");
-			result.Append(description.FieldNameString);
-			result.AppendLine(";");
+			if (CSharpLexer.IsKeyword(name.GetText())) Result.Append("@");
+			Result.AppendMapped(name);
+			Result.Append(" => ");
+			Result.Append(description.FieldNameString);
+			Result.AppendLine(";");
+		}
+
+		protected override void AppendIndent(int size)
+		{
+			// TODO: use user indents?
+			for (int index = 0; index < 10; index += 1)
+			{
+				Result.Append("    ");
+			}
 		}
 	}
 }
