d5584ab140872de641233895e54248d9451c4fba
Kirill <kirillgla@gmail.com>
2019-07-15 10:57:34 +0300

    Format language definition

--- Frontend/src/main/kotlin/com/jetbrains/fortea/language/T4Language.kt
+++ Frontend/src/main/kotlin/com/jetbrains/fortea/language/T4Language.kt
@@ -3,7 +3,5 @@ package com.jetbrains.fortea.language
 import com.jetbrains.rider.ideaInterop.fileTypes.RiderLanguageBase
 
 object T4Language : RiderLanguageBase("T4", "T4") {
-    override fun isCaseSensitive(): Boolean = true
+  override fun isCaseSensitive(): Boolean = true
 }
-
-
