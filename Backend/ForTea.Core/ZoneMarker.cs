b9867b38519cc326ce33c1e8eb790956ac5b84f2
Kirill <kirillgla@gmail.com>
2019-07-16 23:03:55 +0300

    Add zone marker to core project

---
+++ Backend/ForTea.Core/ZoneMarker.cs
@@ -0,0 +1,18 @@
+using JetBrains.Application.BuildScript.Application.Zones;
+using JetBrains.ReSharper.Feature.Services;
+using JetBrains.ReSharper.Feature.Services.Daemon;
+using JetBrains.ReSharper.Feature.Services.Navigation;
+using JetBrains.ReSharper.Psi;
+using JetBrains.ReSharper.Psi.CSharp;
+
+namespace GammaJul.ForTea.Core
+{
+	[ZoneMarker]
+	public class ZoneMarker : IPsiLanguageZone,
+		IRequire<ILanguageCSharpZone>,
+		IRequire<ICodeEditingZone>,
+		IRequire<DaemonZone>,
+		IRequire<NavigationZone>
+	{
+	}
+}
