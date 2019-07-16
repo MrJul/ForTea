7e194e184e8badc3d936c03fa0022bbca476b91e
Kirill <kirillgla@gmail.com>
2019-07-16 17:35:24 +0300

    Update lexer tests

--- Backend/test/ForTea.Tests/Psi/T4LexerTest.cs
+++ Backend/test/ForTea.Tests/Psi/T4LexerTest.cs
@@ -1,5 +1,8 @@
+using GammaJul.ForTea.Core.Parsing;
 using GammaJul.ForTea.Core.Psi;
+using JetBrains.ReSharper.Psi.Parsing;
 using JetBrains.ReSharper.TestFramework;
+using JetBrains.Text;
 using NUnit.Framework;
 
 namespace JetBrains.ForTea.Tests.Psi
@@ -8,10 +11,11 @@ namespace JetBrains.ForTea.Tests.Psi
 	public sealed class T4LexerTest : LexerTestBase
 	{
 		protected override string RelativeTestDataPath => @"Psi\Lexer";
+		protected override ILexer CreateLexer(IBuffer buffer) => new T4Lexer(buffer);
 
 		[TestCase("Simple")]
 		[TestCase("CSharpCode")]
-		[TestCase("VBCode")] // TODO: figure out why it recognizes C# tokens
+		[TestCase("VBCode")]
 		public void TestLexer(string name) => DoOneTest(name);
 	}
 }
