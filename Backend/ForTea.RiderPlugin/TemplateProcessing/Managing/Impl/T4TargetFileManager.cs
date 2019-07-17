5be2a3c7cbf9f4b2075db76cfd2f97510a15d497
Kirill <kirillgla@gmail.com>
2019-07-17 12:38:52 +0300

    Fix transaction not being committed

--- Backend/ForTea.RiderPlugin/TemplateProcessing/Managing/Impl/T4TargetFileManager.cs
+++ Backend/ForTea.RiderPlugin/TemplateProcessing/Managing/Impl/T4TargetFileManager.cs
@@ -3,6 +3,7 @@ using GammaJul.ForTea.Core.Psi.Directives;
 using GammaJul.ForTea.Core.TemplateProcessing;
 using GammaJul.ForTea.Core.Tree;
 using JetBrains.Annotations;
+using JetBrains.Application.Progress;
 using JetBrains.Application.Threading;
 using JetBrains.Diagnostics;
 using JetBrains.ProjectModel;
@@ -49,6 +50,7 @@ namespace JetBrains.ForTea.RiderPlugin.TemplateProcessing.Managing.Impl
 				if (!cookie.CanAddFile(folder, targetLocation, out string reason))
 					throw new InvalidOperationException(reason);
 				cookie.AddFile(folder, targetLocation);
+				cookie.Commit(NullProgressIndicator.Create());
 			});
 		}
 
