a41bf90464e97841119582d9e57a021ad96f913f
Kirill <kirillgla@gmail.com>
2019-07-22 14:19:42 +0300

    Fail more gracefully when unable to resolve macros

--- Backend/ForTea.RiderPlugin/Psi/Resolve/Macros/T4MacroResolver.cs
+++ Backend/ForTea.RiderPlugin/Psi/Resolve/Macros/T4MacroResolver.cs
@@ -3,7 +3,6 @@ using System.Linq;
 using GammaJul.ForTea.Core.Psi.Resolve.Assemblies;
 using GammaJul.ForTea.Core.Psi.Resolve.Macros;
 using JetBrains.Annotations;
-using JetBrains.Diagnostics;
 using JetBrains.ProjectModel;
 using JetBrains.ProjectModel.MSBuild;
 using JetBrains.ProjectModel.Properties;
@@ -21,33 +20,23 @@ namespace JetBrains.ForTea.RiderPlugin.Psi.Resolve.Macros
 			IProjectFile file
 		)
 		{
-			var project = file.GetProject().NotNull();
-			return new Dictionary<string, string>
+			var result = new Dictionary<string, string>
 			{
-				{"Configuration", project.ProjectProperties.ActiveConfigurations.Configurations.Single().Name},
 //			{"DevEnvDir", null},
 //			{"FrameworkDir", null},
 //			{"FrameworkSDKDir", null},
 //			{"FrameworkVersion", null},
 //			{"FxCopDir", null},
 //			{"IntDir", null},
-				{"OutDir", project.GetOutputFilePath(project.GetCurrentTargetFrameworkId()).Parent.FullPath},
 //			{"Platform", null},
 //			{"PlatformShortName", null},
-				{"ProjectDir", project.Location.Parent.FullPath},
 //			{"ProjectExt", null},
-				{"ProjectFileName", project.ProjectFileLocation.Name},
-				{"ProjectName", project.Name},
-				{"ProjectPath", project.Location.FullPath},
 //			{"RemoteMachine", null},
-				{"RootNameSpace", project.GetUniqueRequestedProjectProperty(MSBuildProjectUtil.RootNamespaceProperty)},
 				{"SolutionDir", Solution.SolutionDirectory.FullPath},
 //			{"SolutionExt", null},
-//			{"SolutionFileName", null},
 				{"SolutionName", Solution.Name},
 				{"SolutionPath", Solution.SolutionFilePath.FullPath},
 //			{"TargetDir", null},
-				{"TargetExt", T4MSBuildProjectUtil.GetTargetExtension(project)},
 //			{"TargetFileName", null},
 //			{"TargetName", null},
 //			{"TargetPath", null},
@@ -56,6 +45,21 @@ namespace JetBrains.ForTea.RiderPlugin.Psi.Resolve.Macros
 //			{"WebDeployPath", null},
 //			{"WebDeployRoot", null}
 			};
+			var solutionFile = Solution.SolutionFile;
+			if (solutionFile != null) result.Add("SolutionFileName", solutionFile.Name);
+			var project = file.GetProject();
+			if (project == null) return result;
+
+			result.Add("Configuration", project.ProjectProperties.ActiveConfigurations.Configurations.Single().Name);
+			result.Add("OutDir", project.GetOutputFilePath(project.GetCurrentTargetFrameworkId()).Parent.FullPath);
+			result.Add("ProjectDir", project.Location.Parent.FullPath);
+			result.Add("ProjectFileName", project.ProjectFileLocation.Name);
+			result.Add("ProjectName", project.Name);
+			result.Add("ProjectPath", project.Location.FullPath);
+			result.Add("RootNameSpace",
+				project.GetUniqueRequestedProjectProperty(MSBuildProjectUtil.RootNamespaceProperty));
+			result.Add("TargetExt", T4MSBuildProjectUtil.GetTargetExtension(project));
+			return result;
 		}
 
 		public T4MacroResolver([NotNull] ISolution solution, [NotNull] IT4AssemblyNamePreprocessor preprocessor) :
